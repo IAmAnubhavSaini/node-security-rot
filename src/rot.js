c00aece7c592321a8cb972f66ca877d1f4fc0920
Anubhav <iamanubhavsaini@gmail.com>
2021-01-21 11:19:39 +0530

    Using rot13n and rotations function

--- src/rot.js
+++ src/rot.js
@@ -1,6 +1,7 @@
 const rot13 = require('./rot13')
 const rot47 = require('./rot47')
+const { rot13n, rotations } = require('./rot13n')
 
 module.exports = {
-    rot13, rot47
+    rot13, rot47, rot13n, rotations
 }
